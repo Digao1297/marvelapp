17e0a2b2d600a27d000ab69f6a64b560554f7f6e
digao1297 <digao1297@gmail.com>
2022-10-24 16:03:57 -0300

    🚀 Modulo network com as DIs

--- app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.example.marvelapp.framework.di
 
+import com.example.core.data.network.interceptor.AuthorizationInterceptor
 import com.example.marvelapp.BuildConfig
 import dagger.Module
 import dagger.Provides
@@ -9,11 +10,14 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private const val TIMEOUT_SECONDS = 15L
 
     @Provides
     fun provideRetrofit(
@@ -34,15 +38,25 @@ object NetworkModule {
     }
 
     @Provides
+    fun providerAuthorizationInterceptor(): AuthorizationInterceptor = AuthorizationInterceptor(
+        BuildConfig.PUBLIC_KEY,
+        BuildConfig.PRIVATE_KEY,
+        Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    )
+
+    @Provides
     fun providerOkHttpClient(
-        loggingInterceptor: HttpLoggingInterceptor
+        loggingInterceptor: HttpLoggingInterceptor,
+        authorizationInterceptor: AuthorizationInterceptor
     ): OkHttpClient =
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
-            .readTimeout(15, TimeUnit.SECONDS)
-            .connectTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(authorizationInterceptor)
+            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
             .build()
 
     @Provides
     fun providerGsonConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
 }
