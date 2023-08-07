abbb9a6d166db2bf4542324ce8f689c5bfe6535f
Rodrigo <digao1297@gmail.com>
2023-08-07 07:18:11 -0300

    🚀 implementado layout da tela de favorito

--- app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
@@ -2,18 +2,29 @@ package com.example.marvelapp.presentation.favorites
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentFavoritesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoritesFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
-    }
+    private var _biding: FragmentFavoritesBinding? = null
+    private val binding: FragmentFavoritesBinding get() = _biding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ) = FragmentFavoritesBinding.inflate(
+        inflater,
+        container,
+        false,
+    ).apply {
+        _biding = this
+    }.root
+
+    override fun onDestroy() {
+        _biding = null
+        super.onDestroy()
+    }
 }
